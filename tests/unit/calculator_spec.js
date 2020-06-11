2323a8c8e2914cbad295645ae67d863ac6bb50b2
idmacleod <idmacleod@gmail.com>
2020-06-11 13:38:01 +0100

    Tested arithmetic functions

--- tests/unit/calculator_spec.js
+++ tests/unit/calculator_spec.js
@@ -7,8 +7,32 @@ describe('calculator', function () {
   });
 
   // write unit tests here in the form of "it should do something..."
-  it('it has a sample test', function(){
-    assert.equal(true, true)
-  })
+  it('can add 1 to 4', function () {
+    calculator.previousTotal = 1;
+    calculator.add(4);
+    const actual = calculator.runningTotal;
+    assert.strictEqual(actual, 5);
+  });
+
+  it('can subtract 4 from 7', function () {
+    calculator.previousTotal = 7;
+    calculator.subtract(4);
+    const actual = calculator.runningTotal;
+    assert.strictEqual(actual, 3);
+  });
+
+  it('can multiply 3 by 5', function () {
+    calculator.previousTotal = 3;
+    calculator.multiply(5);
+    const actual = calculator.runningTotal;
+    assert.strictEqual(actual, 15);
+  });
+
+  it('can divide 21 by 7', function () {
+    calculator.previousTotal = 21;
+    calculator.divide(7);
+    const actual = calculator.runningTotal;
+    assert.strictEqual(actual, 3);
+  });
 
 });
